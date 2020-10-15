cfd4a5c879f86f7fe7fb82bc8d03ac4d1b9657e2
Nurfadhil12 <55965350+Nurfadhil12@users.noreply.github.com>
2020-10-16 05:26:15 +0700

    Add files via upload

---
+++ List view/app/src/main/java/com/example/myapplication/Konstanta.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class Konstanta {
+    public static final String DATANAMA = "datanama";
+    public static final String DATAGAMBAR ="datagambar" ;
+}
---
+++ List view/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    String[] namaBuah ={
+            "Alpukat",
+            "Apel",
+            "Ceri",
+            "Durian",
+            "Jambu Air",
+            "Manggis",
+            "Strawberry"
+    };
+    int[] gambarBuah ={
+            R.drawable.alpukat,
+            R.drawable.apel,
+            R.drawable.ceri,
+            R.drawable.durian,
+            R.drawable.jambuair,
+            R.drawable.manggis,
+            R.drawable.strawberry,
+    };
+     int[]suaraBuah ={
+             R.raw.alpukat,
+             R.raw.apel,
+             R.raw.ceri,
+             R.raw.durian,
+             R.raw.jambuair,
+             R.raw.manggis,
+             R.raw.strawberry,
+     };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = findViewById(R.id.ListView);
+
+        ArrayAdapter adapter=new ArrayAdapter (MainActivity.this, android.R.layout.simple_list_item_1, namaBuah);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MediaPlayer.create(MainActivity.this,suaraBuah[position]).start();
+
+                Intent pindah = new Intent(MainActivity.this,DetailActivity.class);
+                pindah.putExtra(Konstanta.DATANAMA,namaBuah[position]);
+                pindah.putExtra(Konstanta.DATAGAMBAR,gambarBuah[position]);
+
+                startActivity(pindah);
+
+            }
+        });
+    }
+}
